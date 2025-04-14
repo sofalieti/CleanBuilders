74298499e36f1275fdba0658326e1ac5945bfab4
Sofalieti <Sofalieti@gmail.com>
2025-04-14 13:32:42 +0300

    Working Variant

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-# Set permissions for Laravel directories
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+# Запуск PHP-FPM в фоновом режиме
+php-fpm &
 
-# permissions for PHPMyAdmin
-mkdir -p /sessions
-
-chmod 777 /sessions
-
-exec "$@"
+# Ожидание завершения всех процессов
+wait
